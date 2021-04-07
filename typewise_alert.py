30a0d0114c7accaeee9b4c08070b54f29a3ac5ec
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-04-08 01:15:52 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -5,11 +5,7 @@ cooling_range =  { 'PASSIVE_COOLING'     : {'lowerLimit': 0,'upperLimit':35},
 alert_messages_for_breach_code = { 'TOO_LOW'  : 'Temperature is too low', 
                                    'TOO_HIGH' : 'Temperature is too high', 
                                    'NORMAL'   : 'Temperature is normal' }
-
-alert_target_function_mapping = { "TO_CONTROLLER": send_to_controller, "TO_EMAIL" : send_to_email, "TO_CONSOLE" : send_to_console }
-  
-                                  
-                                  
+                                
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
     return 'TOO_LOW'
@@ -22,12 +18,6 @@ def classify_temperature_breach(coolingType, temperatureInC):
   lowerLimit = cooling_range[coolingType]['lowerLimit']
   upperLimit = cooling_range[coolingType]['upperLimit']
   return infer_breach(temperatureInC, lowerLimit, upperLimit)
-
-
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType = classify_temperature_breach(batteryChar, temperatureInC)
-  return alert_target_function_mapping[alertTarget](breachType)
-
 
 def send_to_controller(breachType):
   header = 0xfeed
@@ -44,3 +34,9 @@ def send_to_email(breachType):
   print(f'To: {recepient}')
   print(alert_messages_for_breach_code[breachType]) 
   return alert_messages_for_breach_code[breachType]                               
+
+alert_target_function_mapping = { "TO_CONTROLLER": send_to_controller, "TO_EMAIL" : send_to_email, "TO_CONSOLE" : send_to_console }
+
+def check_and_alert(alertTarget, batteryChar, temperatureInC):
+  breachType = classify_temperature_breach(batteryChar, temperatureInC)
+  return alert_target_function_mapping[alertTarget](breachType)
