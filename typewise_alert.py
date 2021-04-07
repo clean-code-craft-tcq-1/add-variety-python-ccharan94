c26bca56c0433e91276bd38885c37f1554a5b1e3
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-04-08 00:29:10 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,8 +1,12 @@
 cooling_range =  { 'PASSIVE_COOLING'     : {'lowerLimit': 0,'upperLimit':35},
                    'HI_ACTIVE_COOLING'   : {'lowerLimit': 0,'upperLimit':45},
-                   'MED_ACTIVE_COOLING'  : {'lowerLimit': 0,'upperLimit':40}
-                 }
+                   'MED_ACTIVE_COOLING'  : {'lowerLimit': 0,'upperLimit':40} }
 
+alert_messages_for_breach_code = { 'TOO_LOW'  : 'Temperature is too low', 
+                                   'TOO_HIGH' : 'Temperature is too high', 
+                                   'NORMAL'   : 'Temperature is normal' }
+                                  
+                                  
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
     return 'TOO_LOW'
@@ -21,22 +25,21 @@ def classify_temperature_breach(coolingType, temperatureInC):
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType = classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
   if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
+    response = send_to_controller(breachType)
+    return response                              
   elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-  return breachType
+    response = send_to_email(breachType)
+    return response
+  return 'ALERT_FAILED'
 
 
 def send_to_controller(breachType):
   header = 0xfeed
-  print(f'{header}, {breachType}')
-
+  print(f'{header}, {breachType}')                             
+  return f'{header}, {breachType}'
 
 def send_to_email(breachType):
   recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+  print(f'To: {recepient}')
+  print(alert_messages_for_breach_code[breachType]) 
+  return alert_messages_for_breach_code[breachType]                               
