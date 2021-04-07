6659f3aedb5ef4bdc24f4e36a6a2681f29c42978
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-04-08 00:33:33 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -4,19 +4,19 @@ import typewise_alert
 
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
-    #Check Breaches
-    self.assertTrue(typewise_alert.infer_breach(10, 20, 60) == 'TOO_LOW')
-    self.assertTrue(typewise_alert.infer_breach(30, 20, 60) == 'TOO_HIGH')
-    self.assertTrue(typewise_alert.infer_breach(50, 20, 60) == 'NORMAL')
+      #Check Breaches
+      self.assertTrue(typewise_alert.infer_breach(10, 20, 60) == 'TOO_LOW')
+      self.assertTrue(typewise_alert.infer_breach(30, 20, 60) == 'TOO_HIGH')
+      self.assertTrue(typewise_alert.infer_breach(50, 20, 60) == 'NORMAL')
    
    #Check Alerts
-   def test_check_and_alert(self): 
-    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', 60) == 'Temperature is too high')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 60) == '65261, TOO_HIGH')
-    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', -5) == 'Temperature is too low')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', -5) == '65261, TOO_LOW')
-    self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', 20) == 'Temperature is normal')
-    self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 20) == '65261, NORMAL')
+  def test_check_and_alert(self): 
+      self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', 60) == 'Temperature is too high')
+      self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 60) == '65261, TOO_HIGH')
+      self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', -5) == 'Temperature is too low')
+      self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', -5) == '65261, TOO_LOW')
+      self.assertTrue(typewise_alert.check_and_alert('TO_EMAIL', 'PASSIVE_COOLING', 20) == 'Temperature is normal')
+      self.assertTrue(typewise_alert.check_and_alert('TO_CONTROLLER', 'PASSIVE_COOLING', 20) == '65261, NORMAL')
     
 if __name__ == '__main__':
   unittest.main()
